73d126321d7ef871cd70ebfa509e47f5977f2f7d
OmkarShelkikar <omkar.shelkikar@gmail.com>
2024-03-22 10:00:50 +0530

    config files

---
+++ e2e/contacttests/Newtest.js
@@ -0,0 +1,34 @@
+///  <reference types="Cypress" />
+
+describe("Test contact us page",() => {
+
+    it("Positive submission",() =>{
+       // cy.visit("https://webdriveruniversity.com/")
+       // cy.get('#contact-us > .thumbnail').click({force:true})
+       // cy.wait(100000)
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Omkar")
+        cy.get('[name="last_name"]').type("Shelkikar")
+        cy.get('[name="email"]').type("omkarsh@gmail.com")
+        cy.get('textarea.feedback-input').type("Any text here")
+        cy.get('[type="submit"]').click()
+    
+    })
+    
+    
+    it.only("Negative submission",() =>{
+    
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Omkar")
+        cy.get('[name="last_name"]').type("Shelkikar")
+        cy.get('textarea.feedback-input').type("Any text here")
+        cy.get('[type="submit"]').click()
+    
+    })
+    
+    
+    
+    
+    
+    
+    })
